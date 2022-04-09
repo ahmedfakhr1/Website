362beb9d0b9b6568e35d9705c18453687dbb84c0
ahmedfakhr1 <84821705+ahmedfakhr1@users.noreply.github.com>
2022-04-09 02:05:45 +0200

    fix in v3

--- app.js
+++ app.js
@@ -1,41 +1,51 @@
-// local reviews data
+﻿// local reviews data
 const reviews = [
   {
-    id: 1,
-    name: "susan smith",
-    job: "web developer",
-    img:
-      "https://res.cloudinary.com/diqqf3eq2/image/upload/v1586883334/person-1_rfzshl.jpg",
-    text:
-      "I'm baby meggings twee health goth +1. Bicycle rights tumeric chartreuse before they sold out chambray pop-up. Shaman humblebrag pickled coloring book salvia hoodie, cold-pressed four dollar toast everyday carry",
+        id: 1,
+        name: "Anna Evgen",
+        job: "Photography Artist",
+        img:
+            "image-anna.jpg",
+        text:
+            "To be honest, i tried some more retouchers.And no one,literally NO ONE did that amazing job as did you!You'tr was very natural,still perfectly edited.very balanced overall!",
+   
   },
   {
     id: 2,
-    name: "anna johnson",
-    job: "web designer",
+    name: "Felicia Boettcher",
+    job: "Certified Makeup Artist ,Fashion, Beauty & Commercial Photography",
     img:
-      "https://res.cloudinary.com/diqqf3eq2/image/upload/v1586883409/person-2_np9x5l.jpg",
+      "image-felica.jpg",
     text:
-      "Helvetica artisan kinfolk thundercats lumbersexual blue bottle. Disrupt glossier gastropub deep v vice franzen hell of brooklyn twee enamel pin fashion axe.photo booth jean shorts artisan narwhal.",
+      "Gorgeous Work!You're So Talented!!",
   },
   {
     id: 3,
-    name: "peter jones",
-    job: "intern",
-    img:
-      "https://res.cloudinary.com/diqqf3eq2/image/upload/v1586883417/person-3_ipa0mj.jpg",
-    text:
-      "Sriracha literally flexitarian irony, vape marfa unicorn. Glossier tattooed 8-bit, fixie waistcoat offal activated charcoal slow-carb marfa hell of pabst raclette post-ironic jianbing swag.",
+      name: "Cerasela Popoviciu",
+      job: "Model",
+      img:
+          "image-cerasela.jpg",
+      text:
+          "Hallo lieber Ahmed,vielen lieben dank!!!Die sind super schön!ich liebe deine Arbeitem so sehr!!!Ich wünsche dir einen schönen Tag.",
   },
   {
     id: 4,
-    name: "bill anderson",
-    job: "the boss",
+    name: "Michael Hoss",
+    job: "Art Director,Photographer",
     img:
-      "https://res.cloudinary.com/diqqf3eq2/image/upload/v1586883423/person-4_t9nxjt.jpg",
+      "image-hoss.jpg",
     text:
-      "Edison bulb put a bird on it humblebrag, marfa pok pok heirloom fashion axe cray stumptown venmo actually seitan. VHS farm-to-table schlitz, edison bulb pop-up 3 wolf moon tote bag street art shabby chic. ",
-  },
+      "Sounds Great!Thanks Ahmed.Beautiful work!",
+    },
+    {
+        id: 5,
+        name: "Radwa Tarek",
+        job: "photographer",
+        img:
+            "image-radwaatarek.jpg",
+        text:
+            "Hi there!So my friend contacted you to retouch a photographs i have taken and GOD,you transferred them to the next level!!So in love with your work!",
+    },
 ];
 
 //select items
